e2618a607ae5ea4e781491bddc4ba77e15a07ace
Alison Cordeiro <acordeiro@gmail.com>
2022-05-09 00:05:06 -0700

    Build componentDidMount function

--- src/index.js
+++ src/index.js
@@ -12,13 +12,14 @@ class App extends React.Component {
     // INITIAL ASSIGNMENT IS THE ONLY TIME WE DIRECTLY ASSIGN TO THIS.STATE
     this.state = { lat: null, errorMessage: '' };
     // EVERY OTHER TIME WE USE setState!
+  }
 
+  // React recommends that you don't do date loading inside constructor.
+  // Instead, put it within componentDidMount - convention.
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // This line won't run yet. Runs later, asynch.
-        this.setState({ lat: position.coords.latitude })
-      },
-      (err) => this.setState({ errorMessage: err.message })
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
   // Never put any work inside the render method, because it's goning to be called all the dang time
